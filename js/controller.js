eba8402a232d5b568017dc9172c3f31bf56e920b
Alan Saber <Alan Saber>
2017-07-13 22:03:21 +0200

    first commit

---
+++ js/controller.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(() => {
+    
+    let date = new Date().toLocaleDateString();
+    let clock = new Date().toLocaleTimeString('en-GB', { hour: "numeric", minute: "numeric" });
+    $("#date").text(date);
+    $("#time").text(clock);
+
+
+});
