63f9eaacaa1cf2f26d3126ed3780a8915d4735d6
Alan Saber <Alan Saber>
2017-07-26 21:10:29 +0200

    small fixes

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,16 @@
 ﻿var views = ["homeView", "loginView"];
+//Date
+let date = new Date().toLocaleDateString();
+let clock = new Date().toLocaleTimeString('en-GB', { hour: "numeric", minute: "numeric" });
 
 $(document).ready(() => {
 
     switchViews("loginView");
 
-    let date = new Date().toLocaleDateString();
-    let clock = new Date().toLocaleTimeString('en-GB', { hour: "numeric", minute: "numeric" });
     $("#date").text(date);
     $("#time").text(clock);
-
+    $(".login").click(function(){
+      switchViews("homeView");
+    });
 
 });
