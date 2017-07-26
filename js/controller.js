a02c91912399d2eb6b9d84e82f69a1a830efac4a
Tjasa Kravcar <pw16l009@technikum-wien.at>
2017-07-26 18:54:52 +0200

    layout login page added

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,8 @@
 
 $(document).ready(() => {
 
-  
+    switchViews("loginView");
+
     let date = new Date().toLocaleDateString();
     let clock = new Date().toLocaleTimeString('en-GB', { hour: "numeric", minute: "numeric" });
     $("#date").text(date);
